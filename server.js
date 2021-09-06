c47e34176983883b2455dbe0ba62a82aa2d60509
yoo-myeong <yoou201@naver.com>
2021-09-06 18:04:11 +0900

    feat : request body를 서버에서 받기
    body-parse를 사용해서 요청의 body를 object형식으로 받을 수 있다.
    이때 server.js에서 console.log로 이 데이터를 출력하면 브라우저에서는 안나옴. form데이터를 서버에서 처리하는 곳이 server.js이기 때문에 서버의 터미널에서만 확인이 가능.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 app.listen(8080, function(){
     console.log('listening on 8080')
 });
+
+app.use(express.urlencoded({extended: true})) 
 // 라이브러리가 서버 만들려면 이렇게 쓰라고 해놨을 뿐 이해하고 사용할 필요없이 기본 셋팅이라고 알아두면 된다.
 
 // listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
@@ -30,3 +32,14 @@ app.get('/', function(req, res){
 app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html');
 })
+
+// listen이나 get 함수는 함수안에 함수를 파라미터로 넣은 콜백함수이다.
+// req, res는 내가 정한 파라미터의 이름인데 첫번째는 요청내용을 담을 것이고, 두번째는 응답할 방법을 담을 것이다.
+
+app.post('/add', function(요청, 응답){
+    응답.send('전송완료');
+    console.log(요청.body)
+})
+
+// 요청이라는 파라미터에 있는 걸 꺼내 쓰려면 body-parser라는 라이브러리를 사용해야 한다.
+//app.use(express.urlencoded({extended: true}))  이 코드를 위에 추가 해주면 사용할 수 있다.
