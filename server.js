d292895945849e4e520ff3da6f795db5246c0717
yoo-myeong <yoou201@naver.com>
2021-09-07 23:12:48 +0900

    feat : mongoDB연결

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, function(){
-    console.log('listening on 8080')
-});
+
 
 app.use(express.urlencoded({extended: true})) 
-// 라이브러리가 서버 만들려면 이렇게 쓰라고 해놨을 뿐 이해하고 사용할 필요없이 기본 셋팅이라고 알아두면 된다.
+// 라이브러리가 서버 만들려면 이렇게 쓰라고 해놨을 뿐 이해하고 사용할 필요없다. 기본 셋팅이라고 알아두면 된다.
 
 // listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://admin:<qwer1234>@cluster0.ejuky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(에러, client){
+    app.listen(8080, function(){
+        console.log('listening on 8080')
+    });
+})
+
+//mongoDB 연결
+
 
 app.get('/pet', function(req, res){
     res.send("펫용품 페이지입니다.");
@@ -43,3 +51,4 @@ app.post('/add', function(요청, 응답){
 
 // 요청이라는 파라미터에 있는 걸 꺼내 쓰려면 body-parser라는 라이브러리를 사용해야 한다.
 //app.use(express.urlencoded({extended: true}))  이 코드를 위에 추가 해주면 사용할 수 있다.
+
